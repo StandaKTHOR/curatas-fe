4664f621c27952370fd7af9b0a5e124cca40a0cc
Stanislav Keppert <stanislav.keppert@systeum.cz>
2026-02-26 21:11:46 +0100

    -- pridani url na railway app as API_BASE

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8080';
+// export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8080';
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://curatas-be-production.up.railway.app';
 
 const getAuthHeader = (): HeadersInit => {
     const token = localStorage.getItem('token');
